fd737c0ef335e52ddc28c3124e052437cdcec19a
Filippo Rossi <rossifilippo2000@gmail.com>
2022-01-18 13:52:17 +0100

    snack1

--- snack1/index.php
+++ snack1/index.php
@@ -3,8 +3,32 @@ Creiamo un array contenente le partite di basket di un’ipotetica tappa del cal
 Olimpia Milano - Cantù | 55-60 -->
 
 <?php
-    
-
+    $partite = [
+        [
+            "squadra1" => "Trieste",
+            "score1" => 83,
+            "squadra2" => "Reggiana",
+            "score2" => 85,
+        ],
+        [
+            "squadra1" => "Brescia",
+            "score1" => 88,
+            "squadra2" => "Brindisi",
+            "score2" => 67,
+        ],
+        [
+            "squadra1" => "Varese",
+            "score1" => 76,
+            "squadra2" => "Trento",
+            "score2" => 80,
+        ],
+        [
+            "squadra1" => "Treviso",
+            "score1" => 70,
+            "squadra2" => "Milano",
+            "score2" => 68,
+        ]
+    ];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,6 +39,13 @@ Olimpia Milano - Cantù | 55-60 -->
     <title>Snack 1</title>
 </head>
 <body>
-    
+    <h1>Risultati:</h1>
+    <p> 
+        <?php
+            for ($i = 0; $i < count($partite); $i++) {
+                echo "<div>" . $partite[$i]['squadra1'] . " - " . $partite[$i]['squadra2'] . " | " . $partite[$i]['score1'] . "-" . $partite[$i]['score2'] . "</div>";
+            } 
+        ?>
+    </p>
 </body>
 </html>
